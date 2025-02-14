ea39ef8b8e2b1789ff0725e9a4d022b05056fa1a
Jaunty <jaunty5899@gmail.com>
2025-02-14 22:47:08 +0530

    final edit

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
       <ClockSegment min={5}>Short Break</ClockSegment>
       <ClockSegment min={15}>Long Break</ClockSegment>
       <ClockSegment min={15}>Long Break</ClockSegment>
+      <ClockSegment min={15}>Long Break</ClockSegment>
     </div>
   );
 }
