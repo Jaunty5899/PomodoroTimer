43316630494780a16397c3c3b41ab5ecbc733333
Jaunty <jaunty5899@gmail.com>
2025-02-28 17:04:36 +0530

    final edit

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,9 @@ import "./App.css";
 function App() {
   return (
     <div className="container">
+      <h1>Pomodoro Clock</h1>
       <ClockSegment min={25}>Focus</ClockSegment>
       <ClockSegment min={5}>Short Break</ClockSegment>
-      <ClockSegment min={15}>Long Break</ClockSegment>
-      <ClockSegment min={15}>Long Break</ClockSegment>
       <ClockSegment min={15}>Long Break</ClockSegment>
     </div>
   );
